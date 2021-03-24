b7bb737f090ccf510d0fbf0888f843bbd6283017
Eddie <eddie.ch.89d@gmail.com>
2021-03-24 09:09:43 +0100

    Initial commit

---
+++ src/test/java/se/lexicon/workshop_mvc/WorkshopMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshop_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
